b12d53536a1a2c56e9ae741e77fb40879d9da855
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-02-12 15:37:01 +0100

    mise en place events avec date post serialisation

---
+++ src/Serializer/Handler/ArticleHandler.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Serializer\Handler;
+
+
+class ArticleHandler
+{
+
+}
--- src/Serializer/Listener/ArticleListener.php
+++ src/Serializer/Listener/ArticleListener.php
@@ -4,7 +4,41 @@
 namespace App\Serializer\Listener;
 
 
-class ArticleListener
+use App\Entity\Article;
+use JMS\Serializer\EventDispatcher\Events;
+use JMS\Serializer\EventDispatcher\EventSubscriberInterface;
+use JMS\Serializer\EventDispatcher\ObjectEvent;
+use JMS\Serializer\Metadata\StaticPropertyMetadata;
+
+class ArticleListener implements EventSubscriberInterface
 {
 
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            [
+                'event' => Events::POST_SERIALIZE, //évènement post serialize à écouter
+                'format' => 'json', //dans quel format la sérialisation doit se passer
+                'class' => Article::class,
+                'method' => 'onPostSerialize', // quelle méthode appeler au moment où le listener est appelé
+            ]
+        ];
+    }
+
+    /**
+     * @param ObjectEvent $event
+     * @var \JMS\Serializer\JsonSerializationVisitor $visitor
+     * @throws \Exception
+     */
+
+    public static function onPostSerialize(ObjectEvent $event)
+    {
+        $date = new \DateTime();
+        //$event->getVisitor()->addData('serialized_at', $date->format('l jS \of F Y h:i:s A'));
+        $visitor = $event->getVisitor();
+        $visitor->visitProperty(new StaticPropertyMetadata('', 'subscribed_at', null ), $date->format('l jS \of F Y h:i:s A'));
+    }
 }
